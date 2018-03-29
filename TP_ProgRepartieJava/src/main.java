f0fe9ea4bcffcd5dac303db38410374efc9dddb6
k16015233 <corentin.kirscher@etu.univ-amu>
2018-03-29 17:35:45 +0200

    Client et Serveur OP

--- TP_ProgRepartieJava/src/main.java
+++ TP_ProgRepartieJava/src/main.java
@@ -3,8 +3,8 @@ import java.io.IOException;
 
 public class main {
     public static void main(String[] args) throws IOException {
-        //ClientTCP clientTCP = new ClientTCP("10.203.9.99", 50007);
-        //clientTCP.lancerBW();
+        ClientTCP clientTCP = new ClientTCP("10.203.9.99", 50007);
+        clientTCP.lancerBW();
         ServeurTCP serveurTCP = new ServeurTCP();
         serveurTCP.lancerServ();
     }
